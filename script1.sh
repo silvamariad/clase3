6f4f38565c0e055f5139a9a405a364fd7b98ac3f
maria silva <m.angelessilvagutierrez@gmail.com>
2022-08-11 21:43:56 -0500

    el archivo script1.sh modificado

--- script1.sh
+++ script1.sh
@@ -15,13 +15,13 @@ mv file2.txt ../empty
 cp foo/dummy/file1.txt foo/empty/file2.txt
 
 #Pasos del Ejercicio 3
-if [ "$1" = "Me encanta bash!!" ]
+if [ ! -z "$1" ]
 then 
-    echo "condicion 1"
-elif [ "$1" = "vacio"  ]
-then
      echo "Que me gusta bash!!!!" >> file1.txt
      echo "Que me gusta bash!!!!" >> file2.txt
      echo "se ha modificado el texto de los ficheros"
-
+    
+elif [ "$1" = "Me encanta bash!!" ]
+then
+    echo "condicion 2 se cumple"
 fi
