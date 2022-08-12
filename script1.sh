2af326325963895b9e721556e2a65cf646d08ba0
maria silva <m.angelessilvagutierrez@gmail.com>
2022-08-11 21:31:28 -0500

    el script del ejercicio 3 terminado

--- script1.sh
+++ script1.sh
@@ -1,1 +1,27 @@
- 
+#!/bin/bash
+#-*- coding: utf-8 -*-
+
+# Pasos del Ejercicio 1 -Linux
+#creo las carpetas del ejercicio1
+mkdir foo
+mkdir foo/dummy
+echo "Me encanta Bash!!!" >> foo/dummy/file1.txt
+echo " " >> foo/dummy/file1.txt
+mkdir foo/empty
+
+#Pasos del Ejercicio 2 
+cd foo/dummy
+mv file2.txt ../empty
+cp foo/dummy/file1.txt foo/empty/file2.txt
+
+#Pasos del Ejercicio 3
+if [ "$1 " = "Me encanta bash!!" ]
+then 
+    echo "condicion 1"
+elif [ "$1" = " "  ]
+then
+     echo "Que me gusta bash!!!!" >> file1.txt
+     echo "Que me gusta bash!!!!" >> file2.txt
+     echo "se ha modificado el texto de los ficheros"
+
+fi
